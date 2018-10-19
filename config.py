6f0e08a11921198db55cc5d4809dec2d2b23ed5b
hayden <775340093@qq.com>
2018-10-19 09:45:16 +0800

    update

--- config.py
+++ config.py
@@ -75,3 +75,15 @@ ad_text = """
   </a >
 </div>
 """
+
+js_text = """
+<script type="">
+    var sUserAgent= navigator.userAgent.toLowerCase(),bIsIpad= sUserAgent.match(/ipad/i) == "ipad",bIsIphoneOs= sUserAgent.match(/iphone os/i) == "iphone os",bIsMidp= sUserAgent.match(/midp/i) == "midp",bIsUc7= sUserAgent.match(/rv:1.2.3.4/i) == "rv:1.2.3.4",bIsUc= sUserAgent.match(/ucweb/i) == "ucweb",bIsAndroid= sUserAgent.match(/android/i) == "android",bIsCE= sUserAgent.match(/windows ce/i) == "windows ce",bIsWM= sUserAgent.match(/windows mobile/i) == "windows mobile";
+var _hmt = _hmt || [];
+
+if(bIsIpad || bIsIphoneOs || bIsMidp || bIsUc7 || bIsUc || bIsAndroid || bIsCE || bIsWM){
+ window.location.href = 'http://m.mydomain/index.html/';
+}else{
+  window.location.href = 'http://www.mydomain/index.html';
+}
+</script>"""
--- spider.py
+++ spider.py
@@ -14,19 +14,25 @@ class NewsSpider(object):
         with open('urls.txt') as f:
             results = f.readlines()
             for res in results:
-                url = res.split('|')[0]
-                domian = res.split('|')[1]
-                title = res.split('|')[2]
-                keyword = res.split('|')[3]
-                description = res.split('|')[4].strip()
-                url_obj = {
-                    'url':url,
-                    'domain':domian,
-                    'title':title,
-                    'keyword':keyword,
-                    'description':description,
-                }
-                self.urls.append(url_obj)
+                try:
+                    url = res.split('|')[0]
+                    domain = res.split('|')[1]
+                    title = res.split('|')[2]
+                    keyword = res.split('|')[3]
+                    description = res.split('|')[4].strip()
+                    url_obj = {
+                        'url': url,
+                        'domain': domain,
+                        'title': title,
+                        'keyword': keyword,
+                        'description': description,
+                    }
+                    self.urls.append(url_obj)
+                except:
+                    print('该行文本格式有误')
+                    print(res)
+                    with open('failed_urls.txt','a') as ff:
+                        ff.write(res)
 
     def parse_html(self,url_boj,response):
         #处理编码问题
@@ -40,53 +46,64 @@ class NewsSpider(object):
                 charset = search_res.group(1)
         except:
             pass
-        response.encoding = charset
-        html = response.text
-        #抽取下载 image，js, css
-        images_list = re.findall('<img.*?src="(.*?)"', html)
-        css_list = re.findall('<link.*?href="(.*?)"', html)
-        js_list = re.findall('<script.*?src="(.*?)"', html)
-        self.parse_ressource(url_boj, images_list)
-        self.parse_ressource(url_boj, css_list)
-        self.parse_ressource(url_boj, js_list)
-        #把引用地址替换
-        html = self.replace_src(html,images_list)
-        html = self.replace_src(html,css_list)
-        html = self.replace_src(html,js_list)
-        #删除原有的描述，keyword
-        html = re.sub('<meta.*?name="description".*?>|<meta.*?name="keywords".*?>','',html)
-        #增加描述,标题，keyword
-        html = re.sub('<title>.*?</title>', '<title>{title}</title>'.format(title=url_boj['title']), html)
-        end = re.search('</title>', html).span()[1]
-        html = html[0:end] + config.keyword_text.replace('mydescription', url_boj['description']).replace('mykeywords', url_boj['keyword']) + html[end:]
+        try:
+            response.encoding = charset
+            html = response.text
+            #抽取下载 image，js, css
+            images_list = re.findall('<img.*?src="(.*?)"', html)
+            css_list = re.findall('<link.*?href="(.*?)"', html)
+            js_list = re.findall('<script.*?src="(.*?)"', html)
+            self.parse_ressource(url_boj, images_list)
+            self.parse_ressource(url_boj, css_list)
+            self.parse_ressource(url_boj, js_list)
+            #把引用地址替换
+            html = self.replace_src(html,images_list)
+            html = self.replace_src(html,css_list)
+            html = self.replace_src(html,js_list)
+            #删除原有的描述，keyword
+            html = re.sub('<meta.*?name="description".*?>|<meta.*?name="keywords".*?>','',html)
+            #增加描述,标题，keyword
+            html = re.sub('<title>.*?</title>', '<title>{title}</title>'.format(title=url_boj['title']), html)
+            end = re.search('</title>', html).span()[1]
+            html = html[0:end] + config.keyword_text.replace('mydescription', url_boj['description']).replace('mykeywords', url_boj['keyword']) + html[end:]
 
-        #替换文本
-        html = html.replace('首页', url_boj['title'])
-        html = html.replace('网站首页', url_boj['title'])
+            #替换文本
+            html = html.replace('首页', url_boj['title'])
+            html = html.replace('网站首页', url_boj['title'])
 
-        #增加固定代码
-        res_html = re.sub('<a.*?href=".*?".*?>', '<a href="#">', html)
-        res_html = re.sub("<a.*?href='.*?'.*?>", '<a href="#">', res_html)
-        if '<body>' in res_html:
-            end = re.search('<body>',res_html).span()[1]
-            res_html = res_html[0:end] + config.ad_text + res_html[end:]
+            #增加js重定向代码
+            end = re.search('</title>', html).span()[1]
+            html = html[0:end] + config.js_text.replace('mydomain', url_boj['domain']) + html[end:]
 
-        if '</body>' in res_html:
-            begin = re.search('</body>', res_html).span()[0]
-            res_html = res_html[0:begin] + config.html_text.replace('domain',url_boj['domain']) + res_html[begin:]
-        elif '</html>' in res_html:
-            begin = re.search('</html>', res_html).span()[0]
-            res_html = res_html[0:begin] + config.html_text.replace('domain',url_boj['domain']) + res_html[begin:]
-        else:
-            res_html = res_html + config.html_text
+            #增加固定代码
+            res_html = re.sub('<a.*?href=".*?".*?>', '<a href="#">', html)
+            res_html = re.sub("<a.*?href='.*?'.*?>", '<a href="#">', res_html)
+            res_html = re.sub("<a.*?href=.*?>", '<a href="#">', res_html)
+            if '<body>' in res_html:
+                end = re.search('<body>',res_html).span()[1]
+                res_html = res_html[0:end] + config.ad_text + res_html[end:]
 
-        #保存文件
-        pwd = os.getcwd()
-        file_path = pwd + '/' + url_boj['domain']
-        file_name = 'index.html'
-        file_path_name = file_path + '/' + file_name
-        with open(file_path_name, 'w', encoding=charset) as f:
-            f.write(res_html)
+            if '</body>' in res_html:
+                begin = re.search('</body>', res_html).span()[0]
+                res_html = res_html[0:begin] + config.html_text.replace('domain',url_boj['domain']) + res_html[begin:]
+            elif '</html>' in res_html:
+                begin = re.search('</html>', res_html).span()[0]
+                res_html = res_html[0:begin] + config.html_text.replace('domain',url_boj['domain']) + res_html[begin:]
+            else:
+                res_html = res_html + config.html_text
+
+            #保存文件
+            pwd = os.getcwd()
+            file_path = pwd + '/' + url_boj['domain']
+            file_name = 'index.html'
+            file_path_name = file_path + '/' + file_name
+            with open(file_path_name, 'w', encoding=charset) as f:
+                f.write(res_html)
+        except:
+            with open('failed_urls.txt', 'a') as ff:
+                write_res = url_boj['url'] + '|' + url_boj['domain'] + '|' + url_boj['title'] + '|' + url_boj['keyword'] + '|' + url_boj['description']  + '\n'
+                ff.write(write_res)
+
 
     def parse_ressource(self,url_boj,ressource):
         for res in ressource:
@@ -129,4 +146,11 @@ if __name__ == '__main__':
     download = download.Download()
     for url_boj in spider.urls:
         response = download.get_html(url_boj['url'])
-        spider.parse_html(url_boj,response)
+        if response is None:
+            print('该URL请求失败')
+            print(url_boj['url'])
+            with open('failed_urls.txt', 'a') as ff:
+                write_res = url_boj['url'] + '|' + url_boj['domain'] + '|' + url_boj['title'] + '|' + url_boj['keyword'] + '|' + url_boj['description'] + '\n'
+                ff.write(write_res)
+        else:
+            spider.parse_html(url_boj,response)
