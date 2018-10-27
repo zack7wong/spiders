45232d82b05f2dccfdb9f51cb4d372fd15467dfb
hayden <775340093@qq.com>
2018-10-27 17:01:48 +0800

    fix

--- baidu_spider/run.py
+++ baidu_spider/run.py
@@ -24,15 +24,15 @@ if __name__ == '__main__':
             spiderone.parse_html(url_boj, response)
 
     #type2
-    # for url_boj in spidertwo.urls:
-    #     print(url_boj)
-    #     # response = download.driver_get_html(url_boj['url'])
-    #     response = download.get_html(url_boj['url'])
-    #     if response is None:
-    #         print('该URL请求失败')
-    #         print(url_boj['url'])
-    #         with open('failed_urls.txt', 'a') as f:
-    #             f.write(str(url_boj) + '\n')
-    #     else:
-    #         spidertwo.parse_html(url_boj, response)
+    for url_boj in spidertwo.urls:
+        print(url_boj)
+        # response = download.driver_get_html(url_boj['url'])
+        response = download.get_html(url_boj['url'])
+        if response is None:
+            print('该URL请求失败')
+            print(url_boj['url'])
+            with open('failed_urls.txt', 'a') as f:
+                f.write(str(url_boj) + '\n')
+        else:
+            spidertwo.parse_html(url_boj, response)
 
