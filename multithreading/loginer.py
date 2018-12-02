1fee8590d79ce74a10f091d632ee6a0dd37934bb
hayden <775340093@qq.com>
2018-12-02 16:52:34 +0800

    update date

--- multithreading/loginer.py
+++ multithreading/loginer.py
@@ -15,7 +15,17 @@ import time
 import requests
 
 def read_txt():
+    success_list = []
     account_list = []
+    try:
+        with open('success.txt') as f:
+            results = f.readlines()
+            for res in results:
+                name = res.split(',')[0]
+                success_list.append(name)
+    except:
+        pass
+
     with open('account.txt') as f:
         results = f.readlines()
         for res in results:
@@ -26,6 +36,8 @@ def read_txt():
                     'name': name,
                     'password': password,
                 }
+                if name in success_list:
+                    continue
                 account_list.append(account_obj)
             except:
                 print('该行文本格式有误')
@@ -35,6 +47,7 @@ def read_txt():
     return account_list
 
 def get_res(account_list,num):
+    thistime = time.strftime('%Y-%m-%d',time.localtime())
     for account in account_list:
         name = account['name']
         password = account['password']
@@ -67,17 +80,24 @@ def get_res(account_list,num):
             json_obj = json.loads(resposne.text)
             if json_obj['errorCode'] == 0:
                 print('登录成功')
-                with open('密码正确.txt','a') as f:
-                    save_res = name + ',' + password + '\n'
+                success_txt = thistime +'-密码正确.txt'
+                with open(success_txt,'a') as f:
+                    save_res = name + '----' + password + '\n'
                     f.write(save_res)
             else:
                 print('登录失败')
-                with open('密码错误.txt', 'a') as f:
-                    save_res = name + ',' + password + '\n'
+                error_txt = thistime + '-密码错误.txt'
+                with open(error_txt, 'a') as f:
+                    save_res = name + '----' + password + '\n'
                     f.write(save_res)
+
+            #写入到测试过的文本
+            with open('success.txt','a') as f:
+                save_res = name + ',' + password + '\n'
+                f.write(save_res)
         except:
             print('未知错误')
-            with open('请求失败.txt', 'a') as f:
+            with open('failed.txt', 'a') as f:
                 save_res = name + ',' + password + '\n'
                 f.write(save_res)
 
@@ -85,6 +105,11 @@ if __name__ == '__main__':
     num = input('请输入进程个数')
     # num = '5'
     account_list = read_txt()
+    if len(account_list) == 0:
+        print('这些账号都验证过了，请更换账号')
+        print('10秒后关闭....')
+        time.sleep(10)
+        exit()
     zhu_num = int(len(account_list)/int(num))
     all_list = [account_list[i:i + zhu_num] for i in range(0, len(account_list), zhu_num)]
 
