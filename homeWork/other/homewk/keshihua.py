788bf85e1b621cba84d32018364d00624ecf884a
hayden <775340093@qq.com>
2018-12-28 12:21:54 +0800

    fix

--- homeWork/other/homewk/keshihua.py
+++ homeWork/other/homewk/keshihua.py
@@ -3,6 +3,12 @@
 
 from pyecharts import Line,configure
 from pyecharts import Bar, Grid
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
+import jieba
+from PIL import Image
+from os import path
+import numpy as np
 
 name_list = []
 price_list = []
@@ -29,3 +35,28 @@ bar.render(path = '条形图.html')
 # grid.add(line, grid_bottom="60%", grid_right="60%")
 # grid.add(bar, grid_bottom="60%", grid_right="60%")
 # grid.render('图表.html')
+
+text = open("results.csv", "rb").read()
+# 结巴分词
+wordlist = jieba.cut(text, cut_all=True)
+wl = " ".join(wordlist)
+
+d = path.dirname(__file__)
+
+alice_mask = np.array(Image.open(path.join(d, "tupian.jpeg")))
+# 设置词云
+wc = WordCloud(background_color="white",  # 设置背景颜色
+               mask = alice_mask,  #设置背景图片
+               max_words=2000,  # 设置最大显示的字数
+               # stopwords = "", #设置停用词
+               font_path="C:\Windows\Fonts\SimHei.ttf",
+               # font_path="/System/Library/Fonts/PingFang.ttc",
+               max_font_size=50,  # 设置字体最大值
+               random_state=30,
+               )
+myword = wc.generate(wl)  # 生成词云
+
+# 展示词云图
+plt.imshow(myword)
+plt.axis("off")
+plt.show()
