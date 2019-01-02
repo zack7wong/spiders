a1351bf6c580e679ab60267e948437e43f1e0e79
hayden <775340093@qq.com>
2019-01-02 16:58:40 +0800

    fix

--- bossZP/chart.py
+++ bossZP/chart.py
@@ -87,12 +87,47 @@ def deal_chengshi_price(results):
     plt.savefig("薪资分布柱状图.jpg")
     plt.show()
 
+def deal_xueli(results):
+    item_list = []
+    account_list = []
+    for res in results:
+        if res[8] in account_list:
+            for item in item_list:
+                if item['key'] == res[8]:
+                    item['value'] += 1
+                    break
+        else:
+            account_list.append(res[8])
+            obj = {
+                'key': res[8],
+                'value': 1
+            }
+            item_list.append(obj)
+
+    item_list = sorted(item_list, key=lambda x: x['value'], reverse=True)
+    name_list = []
+    num_list = []
+    for item in item_list[:10]:
+        name_list.append(item['key'])
+        num_list.append(item['value'])
+
+    # plt.rcParams['font.sans-serif'] = 'SimHei'
+    plt.bar(range(len(num_list)), num_list, tick_label=name_list)
+    plt.savefig("学历分布柱状图.jpg")
+    plt.show()
+
+    plt.pie(num_list, labels=name_list, autopct='%1.2f%%')  # 画饼图
+    plt.savefig("学历分布饼图.jpg")
+    plt.show()
+
+
 def main():
     sql = "select * from positions"
     results = mysqlCli.find_all(sql)
 
     # deal_chengshi(results)
-    deal_chengshi_price(results)
+    # deal_chengshi_price(results)
+    deal_xueli(results)
 
 if __name__ == '__main__':
     mysqlCli = db.MysqlClient()
