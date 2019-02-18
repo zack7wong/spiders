2c3d3aac9b70e7b80442c7243b212d0499c566ae
hayden <775340093@qq.com>
2019-02-18 15:40:16 +0800

    update formhash

--- cyberctm/cyberctm.py
+++ cyberctm/cyberctm.py
@@ -9,7 +9,7 @@ import copy
 from hashlib import md5
 from urllib.parse import quote
 
-USE_PROXY = True
+USE_PROXY = False
 
 proxies = {
     'http':'http://127.0.0.1:1087',
@@ -17,18 +17,20 @@ proxies = {
 }
 
 start_headers = {
-    'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    # 'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+    'Accept': "*/*",
     'Accept-Encoding': "gzip, deflate, br",
     'Accept-Language': "zh-CN,zh;q=0.9",
     'Cache-Control': "no-cache",
     'Connection': "keep-alive",
-    'Cookie': "M1SH_2132_saltkey=TlWmWRlR; M1SH_2132_lastvisit=1547106198; _ga=GA1.2.2010949758.1547109800; M1SH_2132_ulastactivity=58660HwN9pWCdlZV8Rdon0IJ1T5c%2BzeR6PiH8MhG%2Bcaffx092Z4%2B; M1SH_2132_lastcheckfeed=503430%7C1547110183; M1SH_2132_auth=b338n%2FRXnpc%2B5WqnLuAQra7CuV%2B%2F%2BvohgI4AckL6KcGx0yXo31z5khdcqmzvYulHSfMpfMS9%2BWCqUtSs88kExxOAKsk; Forum_auth=458f2lvV3f3Kit62cxprEkDYwVOV7QTEAEkEKkoyn%2FStbaw30e%2B0uSQtVw; M1SH_2132_nofavfid=1; M1SH_2132_visitedfid=291; M1SH_2132_viewid=tid_12344835; M1SH_2132_smile=1D1; __atuvc=2%7C2; M1SH_2132_fastpostrefresh=1; M1SH_2132_st_p=503430%7C1547111029%7C7ace5dc31fff7d2741a4ad9d27bccc47; M1SH_2132_seccode=16612.47f15ac64b4c638123; tracker=afe3VwvLN3swjpwNfAVRa3am5nhgXQM8Ch1HpL6nnaClTWmIc3qP94fE1kNBrMbD7oqDdz2WNvri509ICu6ove6c; _gid=GA1.2.306610996.1547983763; _fbp=fb.1.1547983764864.1116971272; M1SH_2132_checkpm=1; M1SH_2132_lip=13.229.46.217%2C1547984068; M1SH_2132_sendmail=1; _gat=1; M1SH_2132_sid=yLtcx2; M1SH_2132_lastact=1547984070%09misc.php%09patch; _dc_gtm_UA-46392415-16=1; _td=7abfabef-9bfb-4893-90ee-c416eaefb8b1; _gali=pt",
+    # 'Cookie': "M1SH_2132_saltkey=TlWmWRlR; M1SH_2132_lastvisit=1547106198; _ga=GA1.2.2010949758.1547109800; M1SH_2132_ulastactivity=58660HwN9pWCdlZV8Rdon0IJ1T5c%2BzeR6PiH8MhG%2Bcaffx092Z4%2B; M1SH_2132_lastcheckfeed=503430%7C1547110183; M1SH_2132_auth=b338n%2FRXnpc%2B5WqnLuAQra7CuV%2B%2F%2BvohgI4AckL6KcGx0yXo31z5khdcqmzvYulHSfMpfMS9%2BWCqUtSs88kExxOAKsk; Forum_auth=458f2lvV3f3Kit62cxprEkDYwVOV7QTEAEkEKkoyn%2FStbaw30e%2B0uSQtVw; M1SH_2132_nofavfid=1; M1SH_2132_visitedfid=291; M1SH_2132_viewid=tid_12344835; M1SH_2132_smile=1D1; __atuvc=2%7C2; M1SH_2132_fastpostrefresh=1; M1SH_2132_st_p=503430%7C1547111029%7C7ace5dc31fff7d2741a4ad9d27bccc47; M1SH_2132_seccode=16612.47f15ac64b4c638123; tracker=afe3VwvLN3swjpwNfAVRa3am5nhgXQM8Ch1HpL6nnaClTWmIc3qP94fE1kNBrMbD7oqDdz2WNvri509ICu6ove6c; _gid=GA1.2.306610996.1547983763; _fbp=fb.1.1547983764864.1116971272; M1SH_2132_checkpm=1; M1SH_2132_lip=13.229.46.217%2C1547984068; M1SH_2132_sendmail=1; _gat=1; M1SH_2132_sid=yLtcx2; M1SH_2132_lastact=1547984070%09misc.php%09patch; _dc_gtm_UA-46392415-16=1; _td=7abfabef-9bfb-4893-90ee-c416eaefb8b1; _gali=pt",
+    'Cookie': "_ga=GA1.2.2010949758.1547109800; __atuvc=2%7C2%2C0%7C3%2C18%7C4; _gid=GA1.2.1662820720.1550463292; _fbp=fb.1.1550463293074.567664784; M1SH_2132_saltkey=VPA8WbhP; M1SH_2132_lastvisit=1550469032; M1SH_2132_sid=lLOoOq; tracker=6cb7%2F0sgI6ZW6nNHo%2BU3EX6nmvU6CH4ofnFV5SSnhL0UAmR%2F67SQlkJyb%2F5eXPGeyJ2NNTPBjdce350; M1SH_2132_sendmail=1; M1SH_2132_seccode=12531.23d08fcb9b430b7857; M1SH_2132_ulastactivity=c3dciqy8mQ5bgZSNANbaX4gS4b5HMu%2FCaHygQo9%2BYLT9XYU6r3aq; M1SH_2132_lastcheckfeed=503430%7C1550472639; M1SH_2132_checkfollow=1; M1SH_2132_lip=13.250.7.111%2C1550472632; M1SH_2132_auth=b2dbR2zxnvMjdHW844Gji9U%2BGvRhxRN%2BBeK54lOHmebWsDcsSdBXhmDC0cj5FFgqsWgD9mQ78EVqsxgxA%2FOK61gxFOc; Forum_auth=ea7c09jwPY9ib2D34j6ApuXHstWxOwRino%2BPm1GIpD8emVF3x8fAiWGE3g; M1SH_2132_checkpm=1; _td=7abfabef-9bfb-4893-90ee-c416eaefb8b1; M1SH_2132_lastact=1550472643%09misc.php%09patch; _gali=pt",
     'Host': "forum.cyberctm.com",
     'Pragma': "no-cache",
-    'Referer': "https://forum.cyberctm.com/space-uid-503430.html",
-    'Upgrade-Insecure-Requests': "1",
+    # 'Referer': "https://forum.cyberctm.com/space-uid-503430.html",
+    'Referer': "https://forum.cyberctm.com",
+    # 'Upgrade-Insecure-Requests': "1",
     'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
-    'cache-control': "no-cache",
 }
 
 
@@ -117,30 +119,41 @@ def doing(seccode,headers,link,mycomment):
     captcha_res = captcha_res['Result']
 
     # check 验证码
-    check_url = 'https://forum.cyberctm.com/misc.php?mod=seccode&action=check&inajax=1&modid=forum::viewthread&idhash={seccode}&secverify={captcha_res}'
+    headers['X-Requested-With'] = 'XMLHttpRequest'
+    check_url_START = 'https://forum.cyberctm.com/misc.php?mod=seccode&action=check&inajax=1&modid=forum::viewthread&idhash={seccode}&secverify={captcha_res}'
+    check_url = check_url_START.format(seccode=seccode,captcha_res=captcha_res)
+    # print(check_url)
+    # print(headers)
     if USE_PROXY:
         check_response = requests.get(check_url, headers=headers, proxies=proxies, timeout=10)
     else:
         check_response = requests.get(check_url, headers=headers, timeout=10)
+    # print(check_response.text)
     check_response_CookieDic = check_response.cookies.get_dict()
     cookieStr = headers['Cookie']
     cookieStr = replace_cooke(cookieStr, check_response_CookieDic)
     headers['Cookie'] = cookieStr
+    del headers['X-Requested-With']
 
     # 开始回复
     tid = re.search('https://forum.cyberctm.com/forum.php\?mod=viewthread&tid=(\d+)&extra=', link).group(1)
     reply_url = 'https://forum.cyberctm.com/forum.php?mod=post&action=reply&fid=291&tid={tid}&extra=&replysubmit=yes&infloat=yes&handlekey=fastpost&inajax=1'.format(tid=tid)
-    body = 'message={message}&seccodehash={seccode}&seccodemodid=forum%3A%3Aviewthread&seccodeverify={seccodeverify}&posttime={posttime}&formhash=3da10717&usesig=1&subject=++'
+    body = 'message={message}&seccodehash={seccode}&seccodemodid=forum%3A%3Aviewthread&seccodeverify={seccodeverify}&posttime={posttime}&formhash=e633004f&usesig=1&subject=%2B%2B'
     posttime = str(int(time.time()) - 1)
     data = body.format(message=quote(mycomment), seccodeverify=captcha_res, posttime=posttime, seccode=seccode)
+    # print(data)
 
     headers['Content-Type'] = "application/x-www-form-urlencoded"
     headers['Content-Length'] = str(len(data))
+
+    # print(reply_url)
+    # print(headers)
+
     if USE_PROXY:
         response = requests.post(reply_url, headers=headers, data=data, proxies=proxies, timeout=10)
     else:
         response = requests.post(reply_url, headers=headers, data=data,timeout=10)
-    print(response.text)
+    # print(response.text)
     if '非常感謝，回覆發佈成功' in response.text:
         print('发布成功！')
     elif '抱歉，您兩次發表間隔少於 10 秒，請稍候再發表' in response.text:
