390bd229a3fe5be02f7b576251a16d586d7d443e
hayden <775340093@qq.com>
2019-02-27 15:16:42 +0800

    add get_company

---
+++ other/pollution/henan/get_company.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import requests
+import json
+
+url = 'http://www.hnep.gov.cn:98/ashx/GetSiteInfo.ashx?type=1&regioncode=&enptype=&enpname=&enpcode=&infoyear=2014'
+response = requests.get(url)
+print(response.text)
+json_obj = json.loads(response.text)
+for data in json_obj:
+    # print(data)
+    EnpName = data['EnpName']
+    EnpCode = data['EnpCode']
+    EnterTypeName = data['EnterTypeName']
+    InfoYear = str(data['InfoYear'])
+
+    start_url = 'http://www.hnep.gov.cn:98/EnpInfo.aspx?EnpCode={EnpCode}&InfoYear={InfoYear}'.format(EnpCode=EnpCode,InfoYear=InfoYear)
+    save_res = EnpName+','+EnpCode+','+EnterTypeName+','+InfoYear+','+start_url+'\n'
+    print(save_res)
+    with open('2014.txt','a') as f:
+        f.write(save_res)
