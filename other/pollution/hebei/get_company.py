9f36bc44b89ca98586313a85feec5947d3ec1ef1
hayden <775340093@qq.com>
2019-03-03 17:01:04 +0800

    add hebei

---
+++ other/pollution/hebei/get_company.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+-------------------------------------------------
+   File Name：     get_company
+   Description :
+   Author :        hayden_huang
+   Date：          2019/3/3 16:57
+-------------------------------------------------
+"""
+
+id_list = []
+with open('id.txt') as f:
+    resutls = f.readlines()
+    for res in resutls:
+        id_list.append(res.strip())
+print(id_list)
+print(len(id_list))
+print(len(set(id_list)))
+
+
+name_list = []
+with open('name.txt') as f:
+    resutls = f.readlines()
+    for res in resutls:
+        name_list.append(res.strip())
+print(name_list)
+print(len(name_list))
+print(len(set(name_list)))
+
+for name,id in zip(name_list,id_list):
+    save_res = name+','+id+'\n'
+    with open('hebei_id.txt','a') as f:
+        f.write(save_res)
